82d1a701650a14c8894850e0bab63e9ae7da83ee
Kaue Jacyntho <kauekaj@gmail.com>
2022-11-04 15:47:06 -0300

    Creating messageApp following ViewCodedescomplicado course

---
+++ MessageApp/MessageApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessageApp
+//
+//  Created by Kaue de Assis Jacyntho on 04/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
