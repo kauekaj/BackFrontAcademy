3016162faf04e65ace48307cacaeafd1219d5f00
Kaue Jacyntho <kauekaj@gmail.com>
2022-12-14 15:35:46 -0300

    Undo Alert test

--- MessageApp/MessageApp/Alert/Alert.swift
+++ MessageApp/MessageApp/Alert/Alert.swift
@@ -7,22 +7,22 @@
 
 import UIKit
 
-//class Alert: NSObject {
-//    
-//    var controller: UIViewController
-//    
-//    init(controller: UIViewController) {
-//        self.controller = controller
-//    }
-//    
-//    func getAlert(title: String, message: String, completion: (() -> Void)? = nil) {
-//        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-//        let cancel = UIAlertAction(title: "OK", style: .cancel) { action in
-//            completion?()
-//        }
-//        alertController.addAction(cancel)
-//        self.controller.present(alertController, animated: true, completion: nil)
-//    }
+class Alert: NSObject {
+
+    var controller: UIViewController
+
+    init(controller: UIViewController) {
+        self.controller = controller
+    }
+
+    func getAlert(title: String, message: String, completion: (() -> Void)? = nil) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "OK", style: .cancel) { action in
+            completion?()
+        }
+        alertController.addAction(cancel)
+        self.controller.present(alertController, animated: true, completion: nil)
+    }
     
     
 }
