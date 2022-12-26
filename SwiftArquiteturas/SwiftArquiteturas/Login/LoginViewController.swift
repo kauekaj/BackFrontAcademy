db93b9d1c0220588028248731cf8942edf7dc25f
Kaue Jacyntho <kauekaj@gmail.com>
2022-12-26 14:52:05 -0300

    Create Login Presenter

--- SwiftArquiteturas/SwiftArquiteturas/Login/LoginViewController.swift
+++ SwiftArquiteturas/SwiftArquiteturas/Login/LoginViewController.swift
@@ -10,29 +10,39 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-        
+    let presenter: LoginPresenter = LoginPresenter()
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        presenter.delegate = self
     }
     
     
     @IBAction func loginButton(_ sender: Any) {
-        let manager = UserManager(business: UserBusiness())
         
         if let email = emailTextField.text,
            let password = passwordTextField.text {
             
-            manager.login(email: email, password: password) { userModel in
-                self.openHomeView()
-            } failureHandler: { error in
-                self.showMessage(title: "Erro", message: error?.localizedDescription ?? "")
-            }
+            let userModel = UserModel(email: email, password: password)
+            
+            presenter.login(userModel: userModel)
         }
-        
     }
+    
+    
+    @IBAction func registerButton(_ sender: Any) {
+        let registerView = self.storyboard?.instantiateViewController(withIdentifier: "RegisterViewController") as! RegisterViewController
+        registerView.modalPresentationStyle = .fullScreen
+        self.present(registerView, animated: true)
+    }
+
+}
+
+extension LoginViewController: LoginPresenterDelegate {
     
     func showMessage(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
@@ -42,19 +52,11 @@ class LoginViewController: UIViewController {
         self.present(alert, animated: true)
     }
     
-    func openHomeView() {
+    func goHome() {
         let homeView = self.storyboard?.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
         homeView.modalPresentationStyle = .fullScreen
         self.present(homeView, animated: true)
+        
     }
     
-    @IBAction func registerButton(_ sender: Any) {
-        self.openRegisterView()
-    }
-    
-    func openRegisterView() {
-        let homeView = self.storyboard?.instantiateViewController(withIdentifier: "RegisterViewController") as! RegisterViewController
-        homeView.modalPresentationStyle = .fullScreen
-        self.present(homeView, animated: true)
-    }
 }
